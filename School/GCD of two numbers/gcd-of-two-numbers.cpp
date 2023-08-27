be713de3ab3e4255fb8ff28fa9b6ce46cc7f5482
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2023-08-27 23:22:01 +0530

    Updated Solution - GfG to GitHub

--- School/GCD of two numbers/gcd-of-two-numbers.cpp
+++ School/GCD of two numbers/gcd-of-two-numbers.cpp
@@ -9,13 +9,14 @@ class Solution
 	public:
     int gcd(int A, int B) 
 	{ 
-	    if(B==0){
-	        return A;
-	    }
-	    else{
-	        return gcd(B,A%B);
-	    }
-	      
+	   if(B==0){
+	       return A;
+	       
+	   }
+	   else{
+	       return gcd(B,A%B);
+	   }
+	  
 	} 
 };
 
