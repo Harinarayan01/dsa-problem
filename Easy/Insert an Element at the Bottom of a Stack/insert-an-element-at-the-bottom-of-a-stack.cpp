7f9e8c8ffdf6ad8e60386a5faad52410ace168c9
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2024-03-24 19:18:31 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Insert an Element at the Bottom of a Stack/insert-an-element-at-the-bottom-of-a-stack.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    stack<int> insertAtBottom(stack<int> st,int x){
+        stack<int>stt;
+        
+        stack<int>p;
+        while(!st.empty()){
+            int h=st.top();
+            stt.push(h);
+            st.pop();
+        }
+        p.push(x);
+        while(!stt.empty()){
+            int h=stt.top();
+            p.push(h);
+            stt.pop();
+        }
+        
+    return p;    
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n;
+        cin>>x;
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            int a;
+            cin>>a;
+            st.push(a);
+        }
+        Solution ob;
+        stack<int> tmp=ob.insertAtBottom(st,x);
+        vector<int> v;
+        while(tmp.size()){
+            v.push_back(tmp.top());tmp.pop();
+        }
+        reverse(v.begin(),v.end());
+        for(int i=0;i<v.size();i++){
+            cout<<v[i];
+            if(i!=v.size()-1){
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
