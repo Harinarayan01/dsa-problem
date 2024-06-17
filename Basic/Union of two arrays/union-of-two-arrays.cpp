f0ab08f44920b5cd72325e9d57d66bd924f5f2a2
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2024-06-17 11:16:58 +0530

    Updated Solution - GfG to GitHub

--- Basic/Union of two arrays/union-of-two-arrays.cpp
+++ Basic/Union of two arrays/union-of-two-arrays.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 // } Driver Code Ends
 //User function template in C++
-
+  
 class Solution{
     public:
     //Function to return the count of number of elements in union of two arrays.
@@ -16,10 +16,9 @@ class Solution{
             st.insert(a[i]);
         }
         for(int i=0;i<m;i++){
+            
             st.insert(b[i]);
         }
-        
-        
         return st.size();
     }
 };
