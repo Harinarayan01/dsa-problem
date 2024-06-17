e2982e3fd9b479fbbd992c168eb2d7e427cea6e7
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2024-06-17 10:17:58 +0530

    Updated Solution - GfG to GitHub

--- Medium/Kadane's Algorithm/kadanes-algorithm.cpp
+++ Medium/Kadane's Algorithm/kadanes-algorithm.cpp
@@ -1,50 +1,47 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-class Solution{
-    public:
+class Solution {
+  public:
     // arr: input array
     // n: size of array
-    //Function to find the sum of contiguous subarray with maximum sum.
-    long long maxSubarraySum(int arr[], int n){
-        
-        long long ans=INT_MIN;
+    // Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n) {
+
         long long sum=0;
+        long long ans=INT_MIN;
         for(int i=0;i<n;i++){
             sum+=arr[i];
-            if(sum>ans){
-                ans=sum;
-            }
+            ans=max(ans,sum);
             if(sum<0){
                 sum=0;
             }
         }
-        return ans;
+    return ans;        
     }
 };
 
 //{ Driver Code Starts.
 
-int main()
-{
-    int t,n;
-    
-    cin>>t; //input testcases
-    while(t--) //while testcases exist
+int main() {
+    int t, n;
+
+    cin >> t;   // input testcases
+    while (t--) // while testcases exist
     {
-        
-        cin>>n; //input size of array
-        
+
+        cin >> n; // input size of array
+
         int a[n];
-        
-        for(int i=0;i<n;i++)
-            cin>>a[i]; //inputting elements of array
-            
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i]; // inputting elements of array
+
         Solution ob;
-        
+
         cout << ob.maxSubarraySum(a, n) << endl;
     }
 }
